f2c219387bdb98971cc6cf13c06dcd4c9f8b2c13
Jake Swanson <jakswa@gmail.com>
2023-12-28 11:42:30 -0500

    day13 part2

--- src/bin/day_13.rs
+++ src/bin/day_13.rs
@@ -1,7 +1,7 @@
 fn main() {
     let input = aoc_2023_rust::helpers::input("13");
     println!("answer: {}", part1(&input));
-    //println!("answer part 2: {}", part2(&input));
+    println!("answer part 2: {}", part2(&input));
 }
 
 fn part1(input: &str) -> usize {
@@ -19,7 +19,17 @@ fn ind(input: &str) -> Option<usize> {
 }
 
 fn part2(input: &str) -> usize {
-    input.len()
+    input.split("\n\n").map(|s| smudge_ind(s).unwrap()).sum()
+}
+
+fn smudge_ind(input: &str) -> Option<usize> {
+    let lines = input
+        .lines()
+        .map(|i| i.to_string())
+        .collect::<Vec<String>>();
+    smudge_reflect_ind(lines)
+        .map(|i| i * 100)
+        .or(smudge_reflect_ind(flip(input)))
 }
 
 fn flip(input: &str) -> Vec<String> {
@@ -38,9 +48,30 @@ fn reflect_ind(lines: Vec<String>) -> Option<usize> {
             if center * 2 < i + 1 {
                 return true;
             }
-            lines[i] == lines[center * 2 - i - 1]
+            count_diff(&lines[i], &lines[center * 2 - i - 1]) == 0
         })
     })
+}
+
+fn smudge_reflect_ind(lines: Vec<String>) -> Option<usize> {
+    (1..lines.len()).find(|center| {
+        (*center..lines.len())
+            .map(|i| {
+                if center * 2 < i + 1 {
+                    return 0;
+                }
+                count_diff(&lines[i], &lines[center * 2 - i - 1])
+            })
+            .sum::<usize>()
+            == 1
+    })
+}
+
+fn count_diff(l1: &str, l2: &str) -> usize {
+    l1.chars()
+        .enumerate()
+        .filter(|(ind, c)| *c != l2.chars().skip(*ind).next().unwrap())
+        .count()
 }
 
 #[cfg(test)]
@@ -70,6 +101,6 @@ mod tests {
 
     #[test]
     fn part2_works() {
-        //assert_eq!(part2(&INPUT), 5);
+        assert_eq!(part2(&INPUT), 400);
     }
 }
